862cf84c8221c037565200c0bc3ee7e8040ae112
MichelleXinHe <hexinbj1989@gmail.com>
2014-08-07 11:07:36 -0400

    Create PriorityQueueMinComparator.java

---
+++ PriorityQueueMinComparator.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class MinComparator implements Comparator<Integer> {
+	@Override
+	public int compare(Integer i, Integer j){
+		if(i<j) return -1;
+		else if(i==j) return 0;
+		else return 1;
+	}
+}
