24961594ddbf9e33339f93b93423b015f82b6f48
MichelleXinHe <hexinbj1989@gmail.com>
2014-08-08 00:05:39 -0400

    Update ReservoirSampling SelectRandomK.java

--- ReservoirSampling SelectRandomK.java
+++ ReservoirSampling SelectRandomK.java
@@ -1,7 +1,9 @@
-//reference: http://www.geeksforgeeks.org/reservoir-sampling/
-
-public class SelectRandomK {
-	public static int[] selectRandomKElements(int[] array, int n, int k){
+//two methods for this problem
+//1. reservoir sampling
+//2. shuffle a deck of cards
+public class Question3SelectRandomK {
+	//reference: http://www.geeksforgeeks.org/reservoir-sampling/
+	public static int[] selectRandomKElements1(int[] array, int n, int k){
 		//n is the total length of the array
 		int[] result=new int[k];
 		if(n<k) return result;
@@ -20,10 +22,21 @@ public class SelectRandomK {
 		}
 		return result;
 	}
+	
+	//this is a method similar to the method to shuffle a deck of cards
+	public static int[] selectRandomKElements2(int[] array, int n, int k){
+		int[] result=new int[k];
+		for(int i=0; i<k;i++){
+			int index=(int)(Math.random()*(n-i)+i);
+			result[i]=array[index];
+			array[index]=array[i];
+		}
+		return result;
+	}
 	public static void main(String[] args) {
 		int[] stream= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 	    int k = 5;
-	    int[] r=selectRandomKElements(stream, stream.length, k);
+	    int[] r=selectRandomKElements2(stream, stream.length, k);
 	    for(int i=0; i<r.length; i++){
 	    	System.out.print(r[i]+" ");
 	    
